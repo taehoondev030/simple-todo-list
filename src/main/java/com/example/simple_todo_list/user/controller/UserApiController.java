60c0301968857f7ccd6df168bb117cee779ab88f
TaeHoon <taehoondev030@gamil.com>
2025-01-11 20:53:41 +0900

    로그아웃 및 로그인/회원가입 페이지 구현

--- src/main/java/com/example/simple_todo_list/user/controller/UserApiController.java
+++ src/main/java/com/example/simple_todo_list/user/controller/UserApiController.java
@@ -2,8 +2,13 @@ package com.example.simple_todo_list.user.controller;
 
 import com.example.simple_todo_list.service.UserService;
 import com.example.simple_todo_list.user.dto.AddUserRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -17,4 +22,9 @@ public class UserApiController {
         return "redirect/login";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
+    }
 }
